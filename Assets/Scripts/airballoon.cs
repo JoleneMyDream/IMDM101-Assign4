775800b29a838d03f046a8ec5b7d0a563346c79a
JoleneMyDream <jolenezhou0914@gmail.com>
2025-12-02 17:43:12 -0500

    camera angle

--- Assets/Scripts/airballoon.cs
+++ Assets/Scripts/airballoon.cs
@@ -2,9 +2,22 @@ using UnityEngine;
 
 public class HotAirBalloon : MonoBehaviour
 {
-    public float riseSpeed = 2f; 
+    public float riseSpeed = 2f;
+  
+
     void Update()
     {
         transform.Translate(Vector3.up * riseSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("stopBalloon"))
+        {
+           
+            enabled = false;
+
+           
+        }
     }
 }
--- Assets/Scripts/balloonCamera.cs
+++ Assets/Scripts/balloonCamera.cs
@@ -2,17 +2,34 @@ using UnityEngine;
 
 public class BalloonCamera : MonoBehaviour
 {
-    [SerializeField] private Transform balloon;  // 
-    [SerializeField] private Vector3 offset = new Vector3(0, 5, -10); 
+    [SerializeField] private Transform balloon;
+
+
+    [SerializeField] private Vector3 offset = new Vector3(0, 5, -10);
+
+
+    [SerializeField] private float horizontalAngle = 0f;
+    [SerializeField] private float verticalAngle = 30f;
+    [SerializeField] private float distance = 10f;
 
     void LateUpdate()
     {
         if (balloon != null)
         {
-            
-            transform.position = balloon.position + offset;
 
-            
+            float hRad = horizontalAngle * Mathf.Deg2Rad;
+            float vRad = verticalAngle * Mathf.Deg2Rad;
+
+
+            Vector3 rotatedOffset = new Vector3(
+                Mathf.Sin(hRad) * Mathf.Cos(vRad),
+                Mathf.Sin(vRad),
+                Mathf.Cos(hRad) * Mathf.Cos(vRad)
+            ) * distance;
+
+            transform.position = balloon.position + rotatedOffset;
+
+
             transform.LookAt(balloon);
         }
     }
